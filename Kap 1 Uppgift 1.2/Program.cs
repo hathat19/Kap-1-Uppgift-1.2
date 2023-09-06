d6d8704fa4e75461372998ba217b16f3b42ac973
humy06001 <humy06001@E-PW0234KS>
2023-09-06 12:56:12 +0200

    Add project files.

---
+++ Kap 1 Uppgift 1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Byter bakgrundsfärg till blå
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //Byter textfärg till vit
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //Skriver ut ett errormeddelande
+            Console.WriteLine(":(");
+            Console.WriteLine("Your PC ran into a problem that it couldn't handle," +
+            /*Ny rad*/ "\nand now it needs to restart.");
+
+            //Stänger ner fönstret när en knapp trycks
+            Console.ReadKey();
+
+        }
+    }
+
+}
